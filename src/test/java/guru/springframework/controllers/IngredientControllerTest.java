ea2fe237d557ae4e9674aaf94593725bc8cfccb7
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2019-02-18 07:51:25 +0300

    Fixed test problem

--- src/test/java/guru/springframework/controllers/IngredientControllerTest.java
+++ src/test/java/guru/springframework/controllers/IngredientControllerTest.java
@@ -137,9 +137,9 @@ public class IngredientControllerTest {
 
     }
 
-  /*  @Test
+    @Test
     public void testDeleteIngredient() throws Exception {
-
+        when(ingredientService.deleteById(anyString(), anyString())).thenReturn(Mono.empty());
         //then
         mockMvc.perform(get("/recipe/2/ingredient/3/delete")
         )
@@ -148,5 +148,5 @@ public class IngredientControllerTest {
 
         verify(ingredientService, times(1)).deleteById(anyString(), anyString());
 
-    }*/
+    }
 }
