43580a86a38a157446772eb652dffcae58e4723b
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2019-03-11 08:01:05 +0300

    Refactored Exception handling to Reactive

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -2,14 +2,17 @@ package guru.springframework.controllers;
 
 import guru.springframework.commands.RecipeCommand;
 
+import guru.springframework.exceptions.NotFoundException;
 import guru.springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.exceptions.TemplateInputException;
 
 
 /**
@@ -80,20 +83,17 @@ public class RecipeController {
         recipeService.deleteById(id);
         return "redirect:/";
     }
-/*
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(NotFoundException.class)
-    public ModelAndView handleNotFound(Exception exception){
+    @ExceptionHandler({NotFoundException.class, TemplateInputException.class})
+    public String handleNotFound(Exception exception, Model model){
 
         log.error("Handling not found exception");
         log.error(exception.getMessage());
 
-        ModelAndView modelAndView = new ModelAndView();
+        model.addAttribute("exception", exception);
 
-        modelAndView.setViewName("404error");
-        modelAndView.addObject("exception", exception);
 
-        return modelAndView;
-    }*/
+        return "404error";
+    }
 
 }
